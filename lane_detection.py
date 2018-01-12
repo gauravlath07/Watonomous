492ac402c0adb79c3ca4c84f290c788d873108da
gauravlath07 <gauravlath1997@gmail.com>
2018-01-12 12:52:54 -0500

    remove faulty file

---
+++ lane_detection.py
@@ -0,0 +1,165 @@
+import cv2
+import os, glob
+import numpy as np
+from moviepy.editor import VideoFileClip
+
+# get input and store it in array
+path = input("Enter the path with test images - ")
+path = path + '/*.jpg'
+test_images = [cv2.imread(path) for path in glob.glob(path)]
+
+'''
+- change the images to hls color scheme
+- it sets the hue, saturation and light to a certain value
+- used to reduce noise
+'''
+def convert_hls(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+
+# extract only white and yellow from the hls colored image
+def select_white_yellow(image):
+    converted = convert_hls(image)
+    # white color mask
+    lower = np.uint8([  0, 200,   0])
+    upper = np.uint8([255, 255, 255])
+    white_mask = cv2.inRange(converted, lower, upper)
+    # yellow color mask
+    lower = np.uint8([ 10,   0, 100])
+    upper = np.uint8([ 40, 255, 255])
+    yellow_mask = cv2.inRange(converted, lower, upper)
+    # combine the mask
+    mask = cv2.bitwise_or(white_mask, yellow_mask)
+    return cv2.bitwise_and(image, image, mask = mask)
+white_yellow_images = list(map(select_white_yellow, test_images))
+
+# image converted to gray sclae so to detect lines
+def convert_gray_scale(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+gray_images = list(map(convert_gray_scale, white_yellow_images))
+
+# gaussian blur applied to smooth lines so that we get better detection
+def apply_smoothing(image, kernel_size=15):
+    return cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
+blurred_images = list(map(lambda image: apply_smoothing(image), gray_images))
+
+# use canny to detect when the color changes rapidly from 255 to 0 for white lines
+def detect_edges(image, low_threshold=50, high_threshold=150):
+    return cv2.Canny(image, low_threshold, high_threshold)
+edge_images = list(map(lambda image: detect_edges(image), blurred_images))
+
+# images showing the region of interest only
+# hardcoded values to only look below the road and not everything above
+def filter_region(image, vertices):
+    mask = np.zeros_like(image)
+    if len(mask.shape)==2:
+        cv2.fillPoly(mask, vertices, 255)
+    else:
+        cv2.fillPoly(mask, vertices, (255,)*mask.shape[2]) # in case, the input image has a channel dimension        
+    return cv2.bitwise_and(image, mask)
+    
+def select_region(image):
+    # make a square like boundary
+    rows, cols = image.shape[:2]
+    bottom_left  = [cols*0.1, rows*0.95]
+    top_left     = [cols*0.4, rows*0.6]
+    bottom_right = [cols*0.9, rows*0.95]
+    top_right    = [cols*0.6, rows*0.6] 
+
+    # set to integer to make it simple
+    vertices = np.array([[bottom_left, top_left, top_right, bottom_right]], dtype=np.int32)
+    return filter_region(image, vertices)
+roi_images = list(map(select_region, edge_images))
+
+'''
+- right now we have black images with lines outlined as images
+- hough will detect it as lines and color it red
+- p.s not sure whats the theory behind it
+'''
+def hough_lines(image):
+    return cv2.HoughLinesP(image, rho=1, theta=np.pi/180, threshold=20, minLineLength=20, maxLineGap=300)
+list_of_lines = list(map(hough_lines, roi_images))
+
+# just draw lines
+def draw_lines(image, lines, color=[255, 0, 0], thickness=2, make_copy=True):
+    if make_copy:
+        image = np.copy(image)
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(image, (x1, y1), (x2, y2), color, thickness)
+    return image
+line_images = []
+for image, lines in zip(test_images, list_of_lines):
+    line_images.append(draw_lines(image, lines))
+
+
+# get the slope of lines and connect them to reduce noise
+def average_slope_intercept(lines):
+    left_lines    = [] # (slope, intercept)
+    left_weights  = [] # (length,)
+    right_lines   = [] # (slope, intercept)
+    right_weights = [] # (length,)
+    
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            if x2==x1:
+                continue # ignore a vertical line
+            slope = (y2-y1)/(x2-x1)
+            intercept = y1 - slope*x1
+            length = np.sqrt((y2-y1)**2+(x2-x1)**2)
+            if slope < 0: # y is reversed in image
+                left_lines.append((slope, intercept))
+                left_weights.append((length))
+            else:
+                right_lines.append((slope, intercept))
+                right_weights.append((length))
+    
+    # add more weight to longer lines    
+    left_lane  = np.dot(left_weights,  left_lines) /np.sum(left_weights)  if len(left_weights) >0 else None
+    right_lane = np.dot(right_weights, right_lines)/np.sum(right_weights) if len(right_weights)>0 else None
+    
+    return left_lane, right_lane # (slope, intercept), (slope, intercept)
+
+# draw lines here
+def make_line_points(y1, y2, line):
+    if line is None:
+        return None 
+    slope, intercept = line
+    
+    # make sure everything is integer as cv2.line requires it
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    y1 = int(y1)
+    y2 = int(y2)
+    return ((x1, y1), (x2, y2))
+
+def lane_lines(image, lines):
+    left_lane, right_lane = average_slope_intercept(lines)
+    y1 = image.shape[0] # bottom of the image
+    y2 = y1*0.6         # slightly lower than the middle
+    left_line  = make_line_points(y1, y2, left_lane)
+    right_line = make_line_points(y1, y2, right_lane)
+    return left_line, right_line
+
+def draw_lane_lines(image, lines, color=[255, 0, 0], thickness=20):
+    # make a separate image to draw lines and combine with the orignal later
+    line_image = np.zeros_like(image)
+    for line in lines:
+        if line is not None:
+            cv2.line(line_image, *line,  color, thickness)
+    # image1 * α + image2 * β + λ
+    # image1 and image2 must be the same shape.
+    return cv2.addWeighted(image, 1.0, line_image, 0.95, 0.0)
+lane_images = []
+for image, lines in zip(test_images, list_of_lines):
+    lane_images.append(draw_lane_lines(image, lane_lines(image, lines)))
+
+
+# ask for save location and save
+save_path = input("Enter the path with test images - ")
+
+i = 0
+for item in lane_images:
+	temp_i = str(i) + '.jpg'
+	cv2.imwrite(os.path.join(save_path , temp_i), item)
+	i += 1
+
